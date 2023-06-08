111f22a0a3be657f682aca189cdd84451383e03c
EnigmaK9 <enigmak9@protonmail.com>
2023-06-08 00:00:20 -0600

    first commit added first problem

---
+++ recyclable-and-low-fat-products.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/studyplan/top-sql-50
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+select product_id
+from Products
+where low_fats = 'Y' and recyclable = 'Y';
+
